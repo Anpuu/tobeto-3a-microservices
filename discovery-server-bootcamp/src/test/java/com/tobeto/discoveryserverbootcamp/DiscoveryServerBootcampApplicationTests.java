6e790af13eb95686ac7ee15de49e316d1875e590
ISMAIL BAYRAKTAR <bayraktarismail00@gmail.com>
2024-03-12 15:46:53 +0300

    dersteki hatalar çözüldü

---
+++ discovery-server-bootcamp/src/test/java/com/tobeto/discoveryserverbootcamp/DiscoveryServerBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobeto.discoveryserverbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
